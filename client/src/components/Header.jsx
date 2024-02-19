c293945b0035ba3f12a958b297b94b81e339113c
dannygallo11 <dannygallo11@gmail.com>
2024-02-19 11:36:38 -0600

    create the header section

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,9 @@
-import {Link} from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { Button, Navbar } from 'flowbite-react'
-import parces_full_logo1 from '../logo/parces_full_logo1.jpg'
 import parces_small_logo from '../logo/parces_small_logo.jpg'
 
 function Header() {
+  const path = useLocation().pathname;
   return (
     <Navbar className='border-b-2'>
       <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl flex'>
@@ -12,17 +12,24 @@ function Header() {
       </Link>
       <div className='flex gap-2 md:order-2'>
         <Link to='/sign-in'>
-          <Button className='bg-blue-800 hover:bg-blue-900 focus:ring-2 focus:ring-blue-500' pill>
+          <Button className='bg-blue-800 hover:bg-blue-900 focus:ring-2 focus:ring-blue-500 text-white' color='hover:bg-blue-900' pill>
             Sign In
           </Button>
         </Link>
+        <Navbar.Toggle />
+      </div>
         <Navbar.Collapse className='px-2 self-center'>
-          <Navbar.Link >
-            <Link className='px-2' to='/'>Home</Link>
-            <Link className='px-2' to='/projects'>Projects</Link>
+          <Navbar.Link active={path === "/"}>
+            <Link to='/'>
+              Home
+            </Link>
+          </Navbar.Link>
+          <Navbar.Link active={path === "/projects"}>
+            <Link to='/projects'>
+              Projects
+            </Link>
           </Navbar.Link>
         </Navbar.Collapse>
-      </div>
     </Navbar>
   )
 }
