a758f7632ad7281add9026226533981eb5834f99
dannygallo11 <dannygallo11@gmail.com>
2024-02-19 20:11:08 -0600

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    a_number: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+}, {timestamps: true});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
