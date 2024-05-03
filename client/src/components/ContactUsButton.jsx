d5d7a7ea2979ecc643262b93589985d483a1a1fd
dannygallo11 <dannygallo11@gmail.com>
2024-05-02 22:02:35 -0500

    add updated google form link

--- client/src/components/ContactUsButton.jsx
+++ client/src/components/ContactUsButton.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 export function ContactUsButton() {
   return (
-    <Link to="https://forms.gle/ryVpQGpVFmZ7vk5e7">
+    <Link to="https://forms.gle/Gpu5XmqJ9WgBALfU9">
         <Button className='h-20 w-48 my-auto mx-auto' color='failure' pill>
         Contact Us
         <HiOutlineArrowRight className="ml-2 h-5 w-5" />
